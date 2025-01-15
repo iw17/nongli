cfccea2bbcfa6d6d7d816c27f8dff43bad56810c
iw17 <iw17@qq.com>
2025-01-15 10:31:28 +0800

    💡 docs(spider): add comments to the spider file

--- data/spider.py
+++ data/spider.py
@@ -11,11 +11,32 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
+
 def value_input(elem: WebElement, value: int) -> None:
+    '''
+    Inputs an integer value into the webdriver element.
+
+    Args:
+        elem (WebElement): in which the input value is filled
+        value (int): number input into the webdriver element
+    '''
+
     elem.clear()
     elem.send_keys(f'{value}\n')
 
+
 def spider_js(url: str, batch: int, out_file: str) -> None:
+    '''
+    Fetches `nongli` data via JavaScript in the webdriver.
+
+    Args:
+        url (str): from where the `nongli` data are fetched
+        batch (int): how many `sui`s displayed per script
+        out_file (str): path to generated raw data text file
+    Raises:
+        WebDriverException: on bad connection to the site
+    '''
+
     with open(out_file, 'w'):
         pass
     MIN: int = -5000
@@ -39,11 +60,13 @@ def spider_js(url: str, batch: int, out_file: str) -> None:
                 raw_file.write(f'{data.text}\n')
     # time.sleep is unnecessary as only one request is sent
 
+
 def main() -> None:
     URL_SXWNL: tp.LiteralString = 'https://sxwnl.com/super'
     here: str = os.path.dirname(__file__)
     out_txt: str = os.path.join(here, 'build', 'raw.txt')
     spider_js(url=URL_SXWNL, batch=500, out_file=out_txt)
+
 
 if __name__ == '__main__':
     main()
