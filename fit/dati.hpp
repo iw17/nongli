6bfab50a4f4e723d2c16dac617d56c7384f8a87d
iw17 <iw17@qq.com>
2025-02-03 22:35:19 +0800

    ✨ feat(fit): complete fittings for various data
    It is worth mentioning that `std::sin` in `<cmath>` is not `constexpr`,
    so I have implemented a set of such trigonometric functions of my own.
    Trig functions, especially `sin` and `tan`, are used to estimate EoT
    (equation of time) contributed by eccentricity of the Earth's orbit and
    the obliquity of the ecliptic. `Bazi` requires real solar time, which is
    determined by the Sun's apparent location, instead of UTC time.

---
+++ fit/dati.hpp
@@ -0,0 +1,109 @@
+#ifndef IW_DATI_HPP
+#define IW_DATI_HPP 20250206L
+
+#include <cstdint>
+
+#include "math.hpp"
+
+namespace iw17 {
+
+struct date {
+    int16_t year;
+    int8_t mon;
+    int8_t day;
+};
+
+constexpr int32_t date_to_uday(date locd) noexcept {
+    int16_t y = locd.year;
+    int8_t m = locd.mon, d = locd.day;
+    if ((m -= 3) < 0) {
+        y -= 1, m += 12;
+    }
+    int32_t days = (153 * m + 2) / 5 + d - 1;
+    if (y >= 1600) {
+        auto c41 = pydivmod<int32_t>(y, 400);
+        days += (c41.quot - 4) * 146097;
+        auto cy4 = cdivmod<int32_t>(c41.rem, 100);
+        days += cy4.quot * 36524;
+        auto y41 = cdivmod<int32_t>(cy4.rem, 4);
+        days += y41.quot * 1461 + y41.rem * 365;
+    } else {
+        auto y41 = cdivmod<int32_t>(y - 1600, 4);
+        days += y41.quot * 1461 + y41.rem * 365;
+        days += 10 * (days < -6347);
+    }
+    return days - 135080;
+}
+
+constexpr auto uday_to_date(int32_t uday) noexcept {
+    int32_t days = uday + 135080;
+    int16_t y = 1600;
+    quotrem<int32_t> y41 = {0, 0};
+    if (days >= 0) {
+        auto c41 = pydivmod<int32_t>(days, 146097);
+        if (c41.rem == 146096) {
+            y += (c41.quot + 1) * 400;
+            return date{y, 2, 29};
+        }
+        y += c41.quot * 400;
+        auto cy4 = cdivmod<int32_t>(c41.rem, 36524);
+        y += cy4.quot * 100;
+        y41 = cdivmod<int32_t>(cy4.rem, 1461);
+    } else {
+        days -= 10 * (days < -6347);
+        y41 = cdivmod<int32_t>(days, 1461);
+    }
+    if (y41.rem == 1460) {
+        y += (y41.quot + 1) * 4;
+        return date{y, 2, 29};
+    }
+    y += y41.quot * 4;
+    auto ym5 = cdivmod<int32_t>(y41.rem, 365);
+    y += ym5.quot;
+    int8_t m = (5 * ym5.rem + 2) / 153;
+    int8_t d = 1 + ym5.rem - (153 * m + 2) / 5;
+    if ((m += 3) > 12) {
+        m -= 12, y += 1;
+    }
+    return date{y, m, d};
+}
+
+struct dati {
+    int16_t year;
+    int8_t mon;
+    int8_t day;
+    int8_t hour;
+    int8_t min;
+    int8_t sec;
+    int8_t zone;
+};
+
+constexpr date dati_to_date(dati zond) noexcept {
+    return date{zond.year, zond.mon, zond.day};
+}
+
+constexpr int64_t dati_to_usec(dati zond) noexcept {
+    date te = dati_to_date(zond);
+    int32_t uday = date_to_uday(te);
+    int32_t dsec = int32_t(3600) * zond.hour + 60 * zond.min + zond.sec;
+    return int64_t(86400) * uday + dsec - int64_t(900) * zond.zone;
+}
+
+constexpr dati usec_to_dati(int64_t usec, int8_t zone) noexcept {
+    int64_t lsec = usec + int64_t(900) * zone;
+    quotrem<int64_t> dh = pydivmod<int64_t>(lsec, 86400);
+    date locd = uday_to_date(dh.quot);
+    quotrem<int32_t> hm = cdivmod<int32_t>(dh.rem, 3600);
+    quotrem<int32_t> ms = cdivmod<int32_t>(ms.rem, 60);
+    int8_t hour = hm.quot, min = ms.quot, sec = ms.rem;
+    return dati{locd.year, locd.mon, locd.day, hour, min, sec, zone};
+}
+
+constexpr dati zone_cast(dati zodt, int8_t zone) noexcept {
+    int64_t usec = dati_to_usec(zodt);
+    return usec_to_dati(usec, zone);
+}
+
+} // namespace iw17
+
+#endif // IW_DATI_HPP
---
+++ fit/nongli.hpp
@@ -0,0 +1,350 @@
+#ifndef IW_NONGLI_HPP
+#define IW_NONGLI_HPP 20250203L
+
+#include "data.hpp"
+#include "dati.hpp"
+#include "math.hpp"
+
+namespace iw17 {
+
+constexpr int64_t uday_to_usec(int32_t uday) noexcept {
+    return int64_t(86400) * uday - 28800;
+}
+
+constexpr int32_t usec_to_uday(int64_t usec) noexcept {
+    return pydiv<int64_t>(usec + 28800, 86400);
+}
+
+struct riqi { // nian, ryue, tian
+    int16_t nian;
+    int8_t ryue, tian;
+};
+
+constexpr int8_t nian_to_run(int16_t nian) noexcept {
+    nian = clip<int16_t>(nian - _data::NIAN_MIN, 0, _data::NIAN_NUM);
+    uint8_t byte = _data::NR_RUNS[nian / 2];
+    int8_t run = (byte >> 4 * (nian & 1)) & 0b1111;
+    return run;
+}
+
+namespace _fit { // NY: nian_to_cyue
+
+constexpr int32_t ny_pred(int16_t nian) noexcept {
+    int64_t plin = _data::NY_COEF[0] * nian + _data::NY_COEF[1];
+    int64_t bfit = _data::NY_COEF[2] * nian + _data::NY_COEF[3];
+    return plin + (bfit >> _data::NY_BITS);
+}
+
+constexpr int32_t ny_resy(int16_t nian) noexcept {
+    nian -= _data::NIAN_MIN;
+    nian = clip<int16_t>(nian, 0, _data::NIAN_NUM);
+    uint8_t byte = _data::NY_RESY[nian / 8];
+    return (byte >> (nian % 8)) & 1;
+}
+
+} // namespace _fit
+
+constexpr int32_t nian_to_cyue(int16_t nian) noexcept {
+    int32_t pred = _fit::ny_pred(nian);
+    int32_t resy = _fit::ny_resy(nian);
+    return pred + resy;
+}
+
+namespace _fit { // YD: cyue_to_uday
+
+constexpr int32_t yd_pred(int32_t cyue) noexcept {
+    int64_t plin = _data::YD_COEF[0] * cyue + _data::YD_COEF[1];
+    int64_t bfit = _data::YD_COEF[2] * cyue + _data::YD_COEF[3];
+    return plin + (bfit >> _data::YD_BITS);
+}
+
+constexpr int32_t yd_resd(int32_t cyue) noexcept {
+    cyue -= _data::CYUE_MIN;
+    cyue = clip<int32_t>(cyue, 0, _data::CYUE_NUM);
+    constexpr int32_t SIZE = sizeof(_data::YD_RESD_0);
+    quotrem<int32_t> yloc = pydivmod<int32_t>(cyue, SIZE);
+    const uint8_t *arrd = _data::YD_ARRD[yloc.quot];
+    uint8_t byte = arrd[yloc.rem / 4];
+    return (byte >> 2 * (yloc.rem % 4)) & 0b0011;
+}
+
+} // namespace _fit
+
+constexpr int32_t cyue_to_uday(int32_t cyue) noexcept {
+    int32_t pred = _fit::yd_pred(cyue);
+    int32_t resd = _fit::yd_resd(cyue);
+    return pred + resd;
+}
+
+constexpr int32_t uday_to_cyue(int32_t uday) noexcept {
+    int32_t bfit = _data::DY_COEF[1] * uday + _data::DY_COEF[2];
+    int32_t pred = _data::DY_COEF[0] + (bfit >> _data::DY_BITS);
+    int32_t pday = cyue_to_uday(pred);
+    return pred - (uday < pday);
+}
+
+constexpr int16_t cyue_to_nian(int32_t cyue) noexcept {
+    int32_t bfit = _data::YN_COEF[1] * cyue + _data::YN_COEF[2];
+    int32_t pred = _data::YN_COEF[0] + (bfit >> _data::YN_BITS);
+    int32_t pyue = nian_to_cyue(pred);
+    return pred - int32_t(cyue < pyue);
+}
+
+constexpr riqi uday_to_riqi(int32_t uday) noexcept {
+    int32_t cyue = uday_to_cyue(uday);
+    int32_t ud01 = cyue_to_uday(cyue);
+    int16_t nian = cyue_to_nian(cyue);
+    int32_t cy01 = nian_to_cyue(nian);
+    int8_t nyue = cyue - cy01;
+    int8_t run = nian_to_run(nian);
+    int8_t ryue = (nyue + (nyue < run)) * 2 + (nyue == run);
+    int8_t tian = uday - ud01 + 1;
+    return riqi{nian, ryue, tian};
+}
+
+constexpr riqi date_to_riqi(date locd) noexcept {
+    int32_t uday = date_to_uday(locd);
+    return uday_to_riqi(uday);
+}
+
+constexpr int32_t riqi_to_uday(riqi rizi) noexcept {
+    int16_t nian = rizi.nian;
+    int8_t ryue = rizi.ryue, tian = rizi.tian;
+    int8_t run = nian_to_run(nian);
+    int8_t nyue = ryue / 2 - (2 * run >= ryue);
+    int32_t cy01 = nian_to_cyue(nian);
+    int32_t cyue = cy01 + nyue;
+    int32_t ud01 = cyue_to_uday(cyue);
+    return ud01 + tian - 1;
+}
+
+constexpr date riqi_to_date(riqi rizi) noexcept {
+    int32_t uday = riqi_to_uday(rizi);
+    return uday_to_date(uday);
+}
+
+enum class jieqi: int8_t {
+    dongzhi,    xiaohan,    dahan,
+    lichun,     yushui,     jingzhe,
+    chunfen,    qingming,   guyu,
+    lixia,      xiaoman,    mangzhong,
+    xiazhi,     xiaoshu,    dashu,
+    liqiu,      chushu,     bailu,
+    qiufen,     hanlu,      shuangjiang,
+    lidong,     xiaoxue,    daxue,
+};
+
+struct shihou { // sui, jie
+    int16_t sui;
+    jieqi jie;
+};
+
+constexpr int32_t shihou_to_cjie(shihou shi) noexcept {
+    return 24 * int32_t(shi.sui) + int32_t(shi.jie);
+}
+
+constexpr shihou cjie_to_shihou(int32_t cjie) noexcept {
+    quotrem<int32_t> shi = pydivmod<int32_t>(cjie, 24);
+    return shihou{int16_t(shi.quot + 1970), jieqi(shi.rem)};
+}
+
+namespace _fit { // SS: shihou_to_usec
+
+constexpr int64_t ss_pred(shihou shi) noexcept {
+    int64_t sui = shi.sui;
+    int64_t plin = _data::SS_CLIN[0] * sui + _data::SS_CLIN[1];
+    const auto &coef = _data::SS_COEF[int8_t(shi.jie)];
+    int64_t pfit = coef[0];
+    // about 1/3 faster than for-cycle
+    pfit = (pfit * sui >> _data::SS_BITS) + coef[1];
+    pfit = (pfit * sui >> _data::SS_BITS) + coef[2];
+    pfit = (pfit * sui >> _data::SS_BITS) + coef[3];
+    pfit = (pfit * sui >> _data::SS_BITS) + coef[4];
+    pfit = (pfit * sui >> _data::SS_BITS) + coef[5];
+    pfit = (pfit * sui >> _data::SS_BITS) + coef[6];
+    return plin + pfit;
+}
+
+constexpr int64_t ss_ress(shihou shi) noexcept {
+    int32_t cjie = shihou_to_cjie(shi);
+    cjie = clip<int32_t>(cjie, 0, _data::CJIE_NUM);
+    constexpr int32_t SIZE = sizeof(_data::SS_RESS_0);
+    quotrem<int32_t> sloc = pydivmod<int32_t>(cjie, SIZE);
+    const uint8_t *arrs = _data::SS_ARRS[sloc.quot];
+    int32_t isub = sloc.rem * 3 / 2;
+    // 0x12, 0x34, 0x56 -> 0x412, 0x563
+    int32_t pair = (arrs[isub + 1] << 8) | arrs[isub];
+    return (sloc.rem & 1) ? (pair >> 4) : (pair & 0x0fff);
+}
+
+} // namespace _fit
+
+constexpr int64_t shihou_to_usec(shihou shi) noexcept {
+    int64_t pred = _fit::ss_pred(shi);
+    int64_t ress = _fit::ss_ress(shi);
+    return pred + ress;
+}
+
+constexpr dati shihou_to_dati(shihou shi) noexcept {
+    int64_t usec = shihou_to_usec(shi);
+    return usec_to_dati(usec, 32);
+}
+
+constexpr int64_t cjie_to_usec(int32_t cjie) noexcept {
+    shihou shi = cjie_to_shihou(cjie);
+    return shihou_to_usec(shi);
+}
+
+constexpr dati cjie_to_dati(int32_t cjie) noexcept {
+    int64_t usec = cjie_to_usec(cjie);
+    return usec_to_dati(usec, 32);
+}
+
+constexpr int32_t usec_to_cjie(int64_t usec) noexcept {
+    int32_t pbit = _data::SJ_COEF[1] * usec >> _data::SJ_BITS;
+    int32_t plin = (pbit + _data::SJ_COEF[2]) >> _data::SJ_BITS;
+    int32_t pred = plin + _data::SJ_COEF[0];
+    shihou pshi = cjie_to_shihou(pred);
+    int64_t psec = shihou_to_usec(pshi);
+    return pred - (usec < psec);
+}
+
+constexpr int32_t dati_to_cjie(dati zond) noexcept {
+    int64_t usec = dati_to_usec(zond);
+    return usec_to_cjie(usec);
+}
+
+constexpr shihou usec_to_shihou(int64_t usec) noexcept {
+    int32_t cjie = usec_to_cjie(usec);
+    return cjie_to_shihou(cjie);
+}
+
+constexpr shihou dati_to_shihou(dati zond) noexcept {
+    int64_t usec = dati_to_usec(zond);
+    return usec_to_shihou(usec);
+}
+
+enum class zodiac: int8_t { // NOT part of `nongli`
+    aries,      taurus,     gemini,
+    cancer,     leo,        virgo,
+    libra,      scorpio,    sagittarius,
+    capricorn,  aquarius,   pisces,
+};
+
+constexpr zodiac jieqi_to_zodiac(jieqi jie) noexcept {
+    int8_t ijie = clip<int8_t>(int8_t(jie), 0, 23);
+    int8_t izod = (ijie - 6 + 24 * (ijie < 6)) / 2;
+    return zodiac(izod);
+}
+
+constexpr zodiac usec_to_zodiac(int64_t usec) noexcept {
+    shihou shi = usec_to_shihou(usec);
+    return jieqi_to_zodiac(shi.jie);
+}
+
+constexpr zodiac dati_to_zodiac(dati zond) noexcept {
+    int64_t usec = dati_to_usec(zond);
+    return usec_to_zodiac(usec);
+}
+
+enum class ganzhi: int8_t { // 10 `tiangan`s, 12 `dizhi`s
+    jia_zi,     yi_chou,    bing_yin,   ding_mao,   wu_chen,
+    ji_si,      geng_wu,    xin_wei,    ren_shen,   gui_you,
+    jia_xu,     yi_hai,     bing_zi,    ding_chou,  wu_yin,
+    ji_mao,     geng_chen,  xin_si,     ren_wu,     gui_wei,
+    jia_shen,   yi_you,     bing_xu,    ding_hai,   wu_zi,
+    ji_chou,    geng_yin,   xin_mao,    ren_chen,   gui_si,
+    jia_wu,     yi_wei,     bing_shen,  ding_you,   wu_xu,
+    ji_hai,     geng_zi,    xin_chou,   ren_yin,    gui_mao,
+    jia_chen,   yi_si,      bing_wu,    ding_wei,   wu_shen,
+    ji_you,     geng_xu,    xin_hai,    ren_zi,     gui_chou,
+    jia_yin,    yi_mao,     bing_chen,  ding_si,    wu_wu,
+    ji_wei,     geng_shen,  xin_you,    ren_xu,     gui_hai,
+};
+
+constexpr ganzhi nian_to_ganzhi(int16_t nian) noexcept {
+    int16_t nord = pymod<int16_t>(nian - 4, 60);
+    return ganzhi(nord);
+}
+
+constexpr ganzhi byue_to_ganzhi(int32_t byue) noexcept {
+    int32_t yord = pymod<int32_t>(byue + 14, 60);
+    return ganzhi(yord);
+}
+
+constexpr ganzhi bday_to_ganzhi(int32_t bday) noexcept {
+    int32_t dord = pymod<int32_t>(bday + 17, 60);
+    return ganzhi(dord);
+}
+
+constexpr ganzhi bshi_to_ganzhi(int64_t rsec) noexcept {
+    int64_t bshi = pydiv<int64_t>(rsec + 3600, 7200);
+    int64_t sord = pymod<int64_t>(bshi + 24, 60);
+    return ganzhi(sord);
+}
+
+struct bazi { // 4 `zhu`s: `nian`, `yue`, `ri`, `shi`
+    ganzhi zhu[4];
+};
+
+namespace _rst { // real solar time: longitude bias and EoT
+
+constexpr double bias_lon(double lon = 120.0) noexcept {
+    return 240.0 * lon;
+}
+
+constexpr double bias_ecc(int64_t usec) noexcept {
+    double ucen = (usec - 9.46728e8) / 3.15576e9;
+    double oecc = (-4.193e-5 + ucen * -1.26e-7) * ucen + 1.67086e-2;
+    double mano = 1.990968753e-7 * usec + 2.46231e-2;
+    double s1ma = sin(mano), s2ma = sin(2.0 * mano);
+    double hour = -oecc * (2.0 * s1ma + oecc * 1.25 * s2ma);
+    return 86400.0 / TWO_PI * hour;
+}
+
+constexpr double bias_obl(int64_t usec, int32_t cjie) noexcept {
+    double ucen = (usec - 9.46728e8) / 3.15576e9;
+    double eobl = (8.73e-9 * ucen + 3.49e-9) * ucen;
+    eobl = (eobl + 2.26893e-4) * ucen + 4.09093e-1;
+    double t1ho = tan_in45d(eobl / 2.0), t2ho = t1ho * t1ho;
+    int32_t oind = pymod<int32_t>(cjie, 12);
+    int32_t lind = cjie - oind % 3;
+    int64_t last = cjie_to_usec(lind);
+    int64_t next = cjie_to_usec(lind + 3);
+    double spha = HALF_PI * (usec - last) / (next - last);
+    double lpha = HALF_PI / 3.0 * (lind % 12);
+    return 86400.0 / TWO_PI * t2ho * -sin(spha + lpha);
+}
+
+constexpr double bias_eot(int64_t usec, int32_t cjie) noexcept {
+    double becc = bias_ecc(usec);
+    double bobl = bias_obl(usec, cjie);
+    return becc + bobl;
+}
+
+} // namespace _rst: real solar time
+
+constexpr bazi usec_to_bazi(int64_t usec, double lon) noexcept {
+    int32_t cjie = usec_to_cjie(usec);
+    double bias_lon = _rst::bias_lon(lon);
+    double bias_eot = _rst::bias_eot(usec, cjie);
+    int64_t rsec = usec + bias_lon + bias_eot;
+    int32_t bday = pydiv<int64_t>(rsec, 86400);
+    int32_t cjie = usec_to_cjie(usec);
+    int32_t byue = (cjie - 3) >> 1;
+    int32_t bsui = 1970 + pydiv<int32_t>(byue, 12);
+    ganzhi nzhu = nian_to_ganzhi(bsui);
+    ganzhi yzhu = byue_to_ganzhi(byue);
+    ganzhi rzhu = bday_to_ganzhi(bday);
+    ganzhi szhu = bshi_to_ganzhi(rsec);
+    return bazi{nzhu, yzhu, rzhu, szhu};
+}
+
+constexpr bazi dati_to_bazi(dati zond, double lon) noexcept {
+    int64_t usec = dati_to_usec(zond);
+    return usec_to_bazi(usec, lon);
+}
+
+} // namespace iw17
+
+#endif // IW_NONGLI_HPP
