c78e7d493215a55bbff3e5164316a119a9880bf4
iw17 <iw17@qq.com>
2025-03-14 19:57:14 +0800

    ✨ feat(fit): remove 1582-bias and add `tzinfo`
    In `datetime` in Python and `std::chrono::year_month_day` in C++20, the
    missing 10 days in Oct 1582 are taken out of account. For compatibility,
    the `dati` here neglects the 1582-bias as well. In other words, `dati`
    considers Gregorian calendar only.
    
    And magic numbers, like 32 for UTC+8, are not preferred in C++.

--- fit/dati.hpp
+++ fit/dati.hpp
@@ -14,58 +14,72 @@ struct date {
 constexpr int32_t date_to_uday(date locd) noexcept {
     auto [y, m, d] = locd;
     if ((m -= 3) < 0) {
-        y -= 1;
-        m += 12;
+        y -= 1, m += 12;
     }
     int32_t days = (153 * m + 2) / 5 + d - 1;
-    if (y >= 1600) {
-        auto [c41q, c41r] = math::pydivmod<int32_t>(y, 400);
-        days += (c41q - 4) * 146097;
-        auto [cy4q, cy4r] = math::cdivmod<int32_t>(c41r, 100);
-        days += cy4q * 36524;
-        auto [y41q, y41r] = math::cdivmod<int32_t>(cy4r, 4);
-        days += y41q * 1461 + y41r * 365;
-    } else {
-        auto [y41q, y41r] = math::cdivmod<int32_t>(y - 1600, 4);
-        days += y41q * 1461 + y41r * 365;
-        days += 10 * (days < -6347);
-    }
-    return days - 135080;
+    auto [c41q, c41r] = math::pydivmod<int32_t>(y, 400);
+    days += (c41q - 5) * 146097;
+    auto [cy4q, cy4r] = math::cdivmod<int32_t>(c41r, 100);
+    days += cy4q * 36524;
+    auto [y41q, y41r] = math::cdivmod<int32_t>(cy4r, 4);
+    days += y41q * 1461 + y41r * 365;
+    return days + 11017;
 }
 
 constexpr date uday_to_date(int32_t uday) noexcept {
-    int32_t days = uday + 135080;
-    int16_t y = 1600;
-    math::quotrem<int32_t> y41 = {0, 0};
-    if (days >= 0) {
-        auto [c41q, c41r] = math::cdivmod<int32_t>(days, 146097);
-        if (c41r == 146096) {
-            y += (c41q + 1) * 400;
-            return date{y, 2, 29};
-        }
-        y += c41q * 400;
-        auto [cy4q, cy4r] = math::cdivmod<int32_t>(c41r, 36524);
-        y += cy4q * 100;
-        y41 = math::cdivmod<int32_t>(cy4r, 1461);
-    } else {
-        days -= 10 * (days < -6347);
-        y41 = math::pydivmod<int32_t>(days, 1461);
+    int32_t days = uday - 11017;
+    auto [c41q, c41r] = math::pydivmod<int32_t>(days, 146097);
+    int16_t y = 2000 + c41q * 400;
+    if (c41r == 146096) {
+        return date{int16_t(y + 400), 2, 29};
     }
-    auto [y41q, y41r] = y41;
+    auto [cy4q, cy4r] = math::cdivmod<int32_t>(c41r, 36524);
+    auto [y41q, y41r] = math::cdivmod<int32_t>(cy4r, 1461);
+    y += cy4q * 100 + y41q * 4;
     if (y41r == 1460) {
-        y += (y41q + 1) * 4;
-        return date{y, 2, 29};
+        return date{int16_t(y + 4), 2, 29};
     }
-    y += y41q * 4;
     auto [ym5q, ym5r] = math::cdivmod<int32_t>(y41r, 365);
-    y += ym5q;
     int8_t m = (5 * ym5r + 2) / 153;
     int8_t d = 1 + ym5r - (153 * m + 2) / 5;
     if ((m += 3) > 12) {
-        m -= 12;
-        y += 1;
+        m -= 12, y += 1;
     }
-    return date{y, m, d};
+    return date{int16_t(y + ym5q), m, d};
+}
+
+enum class tzinfo: int8_t {
+    west_1200,  west_1145,  west_1130,  west_1115,
+    west_1100,  west_1045,  west_1030,  west_1015,
+    west_1000,  west_0945,  west_0930,  west_0915,
+    west_0900,  west_0845,  west_0830,  west_0815,
+    west_0800,  west_0745,  west_0730,  west_0715,
+    west_0700,  west_0645,  west_0630,  west_0615,
+    west_0600,  west_0545,  west_0530,  west_0515,
+    west_0500,  west_0445,  west_0430,  west_0415,
+    west_0400,  west_0345,  west_0330,  west_0315,
+    west_0300,  west_0245,  west_0230,  west_0215,
+    west_0200,  west_0145,  west_0130,  west_0115,
+    west_0100,  west_0045,  west_0030,  west_0015,
+    utc, // Universal Time Coordinated
+    east_0015,  east_0030,  east_0045,  east_0100,
+    east_0115,  east_0130,  east_0145,  east_0200,
+    east_0215,  east_0230,  east_0245,  east_0300,
+    east_0315,  east_0330,  east_0345,  east_0400,
+    east_0415,  east_0430,  east_0445,  east_0500,
+    east_0515,  east_0530,  east_0545,  east_0600,
+    east_0615,  east_0630,  east_0645,  east_0700,
+    east_0715,  east_0730,  east_0745,  east_0800,
+    east_0815,  east_0830,  east_0845,  east_0900,
+    east_0915,  east_0930,  east_0945,  east_1000,
+    east_1015,  east_1030,  east_1045,  east_1100,
+    east_1115,  east_1130,  east_1145,  east_1200,
+    east_1215,  east_1230,  east_1245,  east_1300,
+    east_1315,  east_1330,  east_1345,  east_1400,
+};
+
+constexpr int64_t zone_to_offset(tzinfo zone) noexcept {
+    return 900 * (int64_t(zone) - int64_t(tzinfo::utc));
 }
 
 struct dati {
@@ -75,7 +89,7 @@ struct dati {
     int8_t hour;
     int8_t min;
     int8_t sec;
-    int8_t zone;
+    tzinfo zone;
 };
 
 constexpr date dati_to_date(dati zond) noexcept {
@@ -86,12 +100,12 @@ constexpr int64_t dati_to_usec(dati zond) noexcept {
     auto [y, m, d, hh, mm, ss, zz] = zond;
     int32_t uday = date_to_uday(date{y, m, d});
     int32_t dsec = int32_t(3600) * hh + 60 * mm + ss;
-    int32_t zsec = int32_t(900) * zz;
+    int32_t zsec = zone_to_offset(zz);
     return int64_t(86400) * uday + dsec - zsec;
 }
 
-constexpr dati usec_to_dati(int64_t usec, int8_t zone) noexcept {
-    int64_t lsec = usec + int64_t(900) * zone;
+constexpr dati usec_to_dati(int64_t usec, tzinfo zone) noexcept {
+    int64_t lsec = usec + zone_to_offset(zone);
     auto [uday, dsec] = math::pydivmod<int64_t>(lsec, 86400);
     auto [y, m, d] = uday_to_date(uday);
     auto [hh, hm] = math::cdivmod<int32_t>(dsec, 3600);
@@ -99,7 +113,7 @@ constexpr dati usec_to_dati(int64_t usec, int8_t zone) noexcept {
     return dati{y, m, d, int8_t(hh), int8_t(mm), int8_t(ss), zone};
 }
 
-constexpr dati zone_cast(dati zond, int8_t zone) noexcept {
+constexpr dati zone_cast(dati zond, tzinfo zone) noexcept {
     int64_t usec = dati_to_usec(zond);
     return usec_to_dati(usec, zone);
 }
