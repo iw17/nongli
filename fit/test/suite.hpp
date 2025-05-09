cee3e6090189d3a5bcf48d1946237dec3fc46094
iw17 <iw17@qq.com>
2025-05-09 11:21:00 +0800

    🚨 fix(fit): solve MSVC warnings C4996 & C4458
    Warning C4996 says `localtime` is unsafe and suggests `localtime_s`, so
    `localtime_s` is used on MSVC. And the variable `pass` in `test` method
    is renamed in case of shadowing the class member.

--- fit/test/suite.hpp
+++ fit/test/suite.hpp
@@ -30,7 +30,12 @@ uint32_t fail;
 test_suite() noexcept: pass(0), fail(0) {
     char buf[32] = "";
     std::time_t now = std::time(nullptr);
+#if _MSC_VER
+    std::tm stm, *ptm = &stm;
+    localtime_s(ptm, &now);
+#else // _MSC_VER
     std::tm *ptm = std::localtime(&now);
+#endif // _MSC_VER
     std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ptm);
     std::printf("Test suite initialized at %s\n", buf);
 }
@@ -43,12 +48,12 @@ bool test(str_t msg, const Ret &real, Ret (*func)(Args...), Args... args) {
         ", taking %" PRId64 " ns\n",
     };
     auto t0 = std::chrono::steady_clock::now();
-    bool pass = func(args...) == real;
+    bool passed = func(args...) == real;
     auto t1 = std::chrono::steady_clock::now();
     std::FILE *fp = nullptr;
     uint32_t count = this->pass + this->fail + 1;
     const char *suf = ordinal_suffix(count);
-    if (pass) {
+    if (passed) {
         std::fprintf((fp = stdout), FMT[0], count, suf, msg);
         this->pass += 1;
     } else {
@@ -57,7 +62,7 @@ bool test(str_t msg, const Ret &real, Ret (*func)(Args...), Args... args) {
     }
     dur_t dur = std::chrono::duration_cast<dur_t>(t1 - t0);
     std::fprintf(fp, FMT[2], dur.count());
-    return pass;
+    return passed;
 }
 
 uint32_t complete() const noexcept {
