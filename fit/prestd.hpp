2c60ab24b447887f401d50af489548f8edefa345
iw17 <iw17@qq.com>
2025-05-20 21:17:46 +0800

    ✨ feat(suite): add `bit_cast` introduced in C++20
    Since a comparison of integers is much faster than element-wise structs,
    `bit_cast` and `uint_cast` are used for `operator==` for structs with
    compact layout. But for those with non-compact layout and some uninited
    bytes, such casts lead to undefined behaviors.

--- fit/prestd.hpp
+++ fit/prestd.hpp
@@ -1,6 +1,8 @@
 #ifndef IW_PRESTD_HPP
 #define IW_PRESTD_HPP 20250613L
 
+#include <type_traits>
+
 namespace iw17::prestd {
 
 [[noreturn]] void unreachable() {
@@ -9,6 +11,56 @@ namespace iw17::prestd {
 #else // GCC or Clang
     __builtin_unreachable();
 #endif // _MSC_VER
+}
+
+template <class Dest, class Src>
+[[nodiscard]] constexpr std::enable_if_t<
+    sizeof(Dest) == sizeof(Src) &&
+    std::is_trivially_copyable_v<Dest> &&
+    std::is_trivially_copyable_v<Src>,
+Dest> bit_cast(Src val) noexcept {
+    return __builtin_bit_cast(Dest, val);
+}
+
+namespace _sct { // struct
+
+using namespace math::literal;
+
+template <uint64_t N>
+struct uint; // incomplete type
+
+template <>
+struct uint<1_u64> {
+    using type = uint8_t;
+};
+
+template <>
+struct uint<2_u64> {
+    using type = uint16_t;
+};
+
+template <>
+struct uint<4_u64> {
+    using type = uint32_t;
+};
+
+template <>
+struct uint<8_u64> {
+    using type = uint64_t;
+};
+
+} // namespace _sct
+
+// only for types with size 1, 2, 4, 8
+template <uint64_t N>
+using uint = typename _sct::uint<N>::type;
+
+// only for types with compact layout
+template <class Src>
+[[nodiscard]] constexpr std::enable_if_t<
+    std::is_trivially_copyable_v<Src>,
+uint<sizeof(Src)>> uint_cast(Src val) noexcept {
+    return bit_cast<uint<sizeof(Src)>>(val);
 }
 
 } // namespace iw17::prestd
